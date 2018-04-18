d400aa64f11feb72a7d28a412fc319b583b0d39f
Marcus Jun <mjun@calstate.edu>
2018-04-18 23:42:34 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -35,15 +35,15 @@ class Song
     #split_genre_name=split_file[2]
 
     #create new song with a title
+
     new_song=Song.new(split_song_name)
     new_song.artist_name=(split_artist_name)
+    new_song
 
     #The artist_name= method does the work
     #of the following two lines of code
     #new_song.artist=Artist.find_or_create_by_name(split_artist_name)
     #new_song.artist.add_song(split_song_name)
-
-    new_song
 
     ##########################################
     #Code that didn't work
